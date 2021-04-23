a380ff3f12abf2c30b16edc9b09690e9f693473b
saritchi <s.ritchie1@hotmail.com>
2021-04-22 17:37:18 -0700

    First commit + initial ideas

---
+++ script.js
@@ -0,0 +1,16 @@
+// Future Fortune
+// A beautiful, smart, and loving person will be coming into your life.
+// A friend asks only for your time not your money.
+// A lifetime of happiness lies ahead of you.
+// Long life is in store for you.
+
+// Inspirational fortune
+// Don’t just think, act!
+// Failure is the chance to do better next time.
+// Get your mind set – confidence will lead you on.
+// Now is the time to try something new
+
+// Funny Fortune
+// Help! I’m being held prisoner in a chinese bakery!
+// Imagination rules the world.
+// “Welcome” is a powerful word.
