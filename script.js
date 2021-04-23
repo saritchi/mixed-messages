c42c22cf0b6c8d45d0e4361ab3c6318e56013c46
saritchi <s.ritchie1@hotmail.com>
2021-04-22 19:45:19 -0700

    Created fortune and wordBank objects

--- script.js
+++ script.js
@@ -1,16 +1,80 @@
 // Future Fortune
-// A beautiful, smart, and loving person will be coming into your life.
-// A friend asks only for your time not your money.
-// A lifetime of happiness lies ahead of you.
-// Long life is in store for you.
+// {person} will be coming into your life... --> Friends, strangers, dogs, gaming computers
+// A lifetime of {happiness} lies ahead of you. --> sadness, riches, drama, happy memories
+// {Long life} is in store for you. --> puppies, hunger, chores, success
 
 // Inspirational fortune
-// Don’t just think, act!
-// Failure is the chance to do better next time.
-// Get your mind set – confidence will lead you on.
-// Now is the time to try something new
+// Don’t just think, {act}! --> invest, eat, learn, donate
+// Failure is the chance to {do better next time}. --> try again, learn, forgive
+// Now is the time to {try something new} --> treat yourself, confess your love, improve yourself, start coding
 
 // Funny Fortune
-// Help! I’m being held prisoner in a chinese bakery!
-// Imagination rules the world.
-// “Welcome” is a powerful word.
+// Help! I’m being held prisoner by {} --> 
+// {Imagination} rules the world. --> 
+// You laugh now. Wait till you get home to. --> Voldemort, Chicken Nuggets, Noobs, Evil Chickens, Santa Claus, Dwight Schrute, Crying babies, Donald Trump, Steve Urkel
+    // options: [
+    //             ['friend', 'stranger', 'dog', 'gaming computer'],
+    //             ['sadness', 'riches', 'drama', 'happy memories'],
+    //             ['puppies', 'hunger', 'chores', 'success']
+    //          ]
+const wordBank = {
+    _categories: ['future', 'inspirational', 'funny'],
+    _future: ['friends', 'strangers', 'dogs', 'gaming computers', 'sadness', 'riches', 'drama', 'happy memories', 'puppies', 'hunger', 'chores', 'success'],
+    _inspirational: ['invest', 'eat', 'learn', 'donate', 'try again', 'learn', 'forgive', 'treat yourself', 'confess your love', 'improve yourself', 'start coding'],
+    _funny: ['Voldemort', 'chicken nuggets', 'noobs', 'evil chickens', 'Santa Clause', 'Dwight Schrute', 'crying babies', 'Donald Trump', 'Steve Urkel'],
+    generateCategory() {
+        const index = Math.floor(Math.random() * 3);
+        return this._categories[index];
+    },
+    generateWord(category) {
+        let word;
+        switch(category) {
+            case 'future':
+                word = this._future[Math.floor(Math.random() * this._future.length)];
+                break;
+            case 'inspirational':
+                word = this._inspirational[Math.floor(Math.random() * this._inspirational.length)];
+                break; 
+            case 'funny':
+                word = this._funny[Math.floor(Math.random() * this._funny.length)];
+                break; 
+        }
+        return word;
+    }
+}
+const fortunes = {
+    _futureFortunes: [],
+    _inspirationalFortunes: [],
+    _funnyFortunes: [],
+    set futureFortunes(string) {
+        this._futureFortunes = [
+            `${string} will be coming into your life...`, 
+            `A lifetime of ${string} lies ahead of you.`,
+            `${string} is in store for you.`
+           ];
+    },
+    set inspirationalFortunes(string) {
+        this._inspirationalFortunes = [
+            `Don't just think. ${string}!`, 
+            `Failure is the chance to ${string}.`,
+            `Now is the time to ${string}.`
+           ];
+    },
+    set funnyFortunes(string) {
+        this._funnyFortunes = [
+            `Help! I’m being held prisoner by ${string}!`, 
+            `${string} rules the world.`,
+            `You laugh now. Wait till you get home to ${string}.`
+           ];
+    },
+    generateFortune(category) {
+        if(category === 'future'){
+            return this._futureFortunes[Math.floor(Math.random() * 3)]
+        } else if(category == 'inspirational'){
+            return this._inspirationalFortunes[Math.floor(Math.random() * 3)]
+        } else if(category == 'funny'){
+            return this._funnyFortunes[Math.floor(Math.random() * 3)]
+        }
+        
+    }
+};
